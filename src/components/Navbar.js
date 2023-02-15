70b227d193708982bc8b498ccd212983d134f4db
TRK41 <Troyk@bell.net>
2023-02-15 02:25:12 -0500

    toggle button appear in small window

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../pages/css/Navbar.css"
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+    const [expandNavbar,setExpandNavbar] = useState(false);
     return (
         <div className='navbar'>
 
             <div className='toggleButton'>
-                <button></button>
+                <button onClick={() => {setExpandNavbar((prev) => !prev);
+                }}>
+                    <ReorderIcon/>
+                </button>
             </div>
             <div className='links'>
             <Link to="/"> Home</Link>
