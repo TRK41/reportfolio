72f07f4ec55eb1ae6bf0f75bf39053e8316be01a
TRK41 <Troyk@bell.net>
2023-02-15 18:19:18 -0500

    building portfolio page

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Projects({image,name}) {
+  return (
+    <div className='projects'>
+        <div>
+            <h1></h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Projects
