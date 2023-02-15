440b3cceac0f5c8c8963d5422180ef16122ef46f
TRK41 <Troyk@bell.net>
2023-02-14 23:34:13 -0500

    imported pages in to App.js and created components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import { Home } from '@mui/icons-material';
+import  Home  from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function Navbar() {
   return (
     <div>
-      Home
+      
     </div>
   )
 }
 
-export default Home
+export default Navbar
