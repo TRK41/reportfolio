7212c4f5a3ccc0b338688fbf79ea8d8883cc952d
TRK41 <Troyk@bell.net>
2023-02-14 19:31:39 -0500

    Creating Routes for the different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { Home } from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/aboutme" element={<AboutMe/>}/>
+        <Route path="/portfolio" element={<Portfolio/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/resume" element={<Resume/>}/>
+      </Routes>
+    </Router>
       
     </div>
   );
