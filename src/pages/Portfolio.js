e2b715eaa1ae7a6f3eac9f00f96d23cd052d1c95
TRK41 <Troyk@bell.net>
2023-02-15 19:05:32 -0500

    add photos for each project

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Projects from '../components/Projects'
+
 
 function Portfolio() {
   return (
     <div className='protfolios'>
       <h1> My Projects</h1>
-      <div className="portfolioList"></div>
+      <div className="portfolioList">
+      <Projects/>
+      <Projects/>
+      <Projects/>
+      </div>
     </div>
   )
 }
