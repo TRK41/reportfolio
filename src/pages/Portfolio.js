2c424608f3e49f9595105ccc2cce35343af7d252
TRK41 <Troyk@bell.net>
2023-02-15 23:05:29 -0500

    working on different pages and css

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,20 +7,20 @@ import project3 from "../Assets/project3.png"
 import project4 from "../Assets/project4.png"
 import project5 from "../Assets/project5.png"
 import project6 from "../Assets/project6.png"
-import project7 from "../Assets/project7.png"
+// import project7 from "../Assets/project7.png"
 
 function Portfolio() {
   return (
     <div className='portfolios'>
       <h1> My Projects</h1>
       <div className="portfolioList">
-      <Projects name="Horiseon Css layout" image={project1}/>
-      <Projects name="Html portfolio" image={project2}/> 
-      <Projects name="Password Generator" image={project3}/> 
-      <Projects name="Agenda" image={project4}/> 
-      <Projects name="ReadMe Generator" image={project5}/> 
-      <Projects name="Note Taker" image={project6}/> 
-      <Projects name="Team Profiles" image={project7}/> 
+      <Projects name="Horiseon Css layout" image={project1} link= 'https://trk41.github.io/Horiseon-accessibility-project/' ></Projects>
+      <Projects name="Html portfolio" image={project2}></Projects> 
+      <Projects name="Password Generator" image={project3}></Projects>
+      <Projects name="Agenda" image={project4}></Projects> 
+      <Projects name="ReadMe Generator" image={project5}></Projects>
+      <Projects name="Note Taker" image={project6}></Projects>
+      {/* <Projects name="Team Profiles" image={project7}/>  */}
       </div>
     </div>
   )
