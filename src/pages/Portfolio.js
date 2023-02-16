0295a4db56a30cfb9215e6e3be18951d24c574ce
TRK41 <Troyk@bell.net>
2023-02-15 19:58:33 -0500

    rename folder assets and title to the images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Projects from '../components/Projects'
-
+import project1 from "../Assets/project1.jpg"
+import project2 from "../Assets/project2.jpeg"
+import project3 from "../Assets/project3.png"
+import project4 from "../Assets/project4.png"
+import project5 from "../Assets/project5.png"
+import project6 from "../Assets/project6.png"
+import project7 from "../Assets/project7.png"
 
 function Portfolio() {
   return (
     <div className='protfolios'>
       <h1> My Projects</h1>
       <div className="portfolioList">
-      <Projects/>
-      <Projects/>
-      <Projects/>
+      <Projects name="Horiseon Css layout" image={project1}/>
+      <Projects name="Html portfolio" image={project2}/> 
+      <Projects name="Password Generator" image={project3}/> 
+      <Projects name="Agenda" image={project4}/> 
+      <Projects name="ReadMe Generator" image={project5}/> 
+      <Projects name="Note Taker" image={project6}/> 
+      <Projects name="Team Profiles" image={project7}/> 
       </div>
     </div>
   )
